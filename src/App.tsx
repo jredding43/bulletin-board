53e08c47caae87687b7af7c0d4052ba5a4fd8bbd
Jack Redding <jredding2543@gmail.com>
2025-02-17 09:37:55 -0800

    Fixed React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
     
     <AuthProvider>
       <WatchedJobsProvider>
-      <Router>
+      <Router basename="/bulletin-board">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
